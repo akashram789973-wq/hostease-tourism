37b51fd257d2fb6b0640358d41b8e961f030b737
akashram789973-wq <akashram789973@gmail.com>
2026-05-29 05:56:56 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBKgYoRJ0ayyz24EdBY_cE3Le4CxcS6dOs",
+  authDomain: "hostease-tourism.firebaseapp.com",
+  projectId: "hostease-tourism",
+  storageBucket: "hostease-tourism.firebasestorage.app",
+  messagingSenderId: "67540424685",
+  appId: "1:67540424685:web:4e526c80ccfb5f477079e4",
+  measurementId: "G-CFCLX8P48F"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
